83e54b160d1006a8c39b897a5b85894dcdb7b4d2
korneevsergey <seriozhakorneev@gmail.com>
2022-12-18 17:10:49 +0300

    main
    Minor fix

--- displaynumberofevents/main.go
+++ displaynumberofevents/main.go
@@ -38,26 +38,31 @@ func main() {
 	result := make([]output, 1)
 
 	for scanner.Scan() {
-		if len(scanner.Text()) > len(parseLayout) {
-			//	parse string date to golang time,
-			//  also stop runtime and print error if line did not validate format
-			t, err := time.Parse(parseLayout, scanner.Text()[:len(parseLayout)])
-			if err != nil {
-				log.Fatal(err)
-			}
 
-			tMin := t.Truncate(time.Minute)
-			if result[len(result)-1].minuteTime != tMin {
-				result = append(result, output{
-					minuteTime: tMin,
-					count:      0,
-				})
-			}
-
-			if strings.HasSuffix(scanner.Text(), suffix) {
-				result[len(result)-1].count++
-			}
+		// skip empty
+		if len(scanner.Text()) < len(parseLayout) {
+			continue
 		}
+
+		//	parse string date to golang time,
+		//  also stop runtime and print error if line did not validate format
+		t, err := time.Parse(parseLayout, scanner.Text()[:len(parseLayout)])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		tMin := t.Truncate(time.Minute)
+		if result[len(result)-1].minuteTime != tMin {
+			result = append(result, output{
+				minuteTime: tMin,
+				count:      0,
+			})
+		}
+
+		if strings.HasSuffix(scanner.Text(), suffix) {
+			result[len(result)-1].count++
+		}
+
 	}
 
 	for _, v := range result[1:] {
